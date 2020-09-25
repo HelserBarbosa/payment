2b4e73f8ec3f1d90cc3245e500d3b13fb3b81931
Helder Barbosa <helderlb@gmail.com>
2020-09-25 19:31:36 -0300

    payment

---
+++ src/main/java/com/si/dsi/payment/payment/repositories/PaymetRepositories.java
@@ -0,0 +1,8 @@
+package com.si.dsi.payment.payment.repositories;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "payment", path = "payment")
+public interface PaymetRepositories {
+
+}
